31adf1314d8c129bb2115eafabe13e501ac3d2c6
AleexCh08 <40615602+AleexCh08@users.noreply.github.com>
2018-06-28 20:39:15 -0400

    Add files via upload

--- ReportApp/src/Controlador/CTRL_ACCESO.java
+++ ReportApp/src/Controlador/CTRL_ACCESO.java
@@ -1,7 +1,7 @@
 package Controlador;
 
 public class CTRL_ACCESO {
-    public static void main(){
+    public static void main(String[] args){
         
     }
 }
--- ReportApp/src/Controlador/Ctrl_Trabajos.java
+++ ReportApp/src/Controlador/Ctrl_Trabajos.java
@@ -1,5 +1,35 @@
 package Controlador;
 
+import Vista.*;
+import Modelo.*;
+
 public class Ctrl_Trabajos {
+    private static Ctrl_Trabajos uniqueInstance = null;
+    private Ctrl_Trabajos(){
+        iAcceso = new IAcceso();
+        iCargar = new ICargar();
+        iConsulta = new IConsulta();
+        iPrincipalCom = new IPrincipalCom();
+        iReportesCentro = new IReportesCentro();
+        iReportesProf = new IReportesProf();
+        iReportesTrabProf = new IReportesTrabProf();
+        reporte = new Reporte();
+        
+    }
     
+    public static Ctrl_Trabajos getInstance(){
+        if(uniqueInstance == null){
+            uniqueInstance = new Ctrl_Trabajos();
+        }
+        return uniqueInstance;
+    }
+    
+    private IAcceso iAcceso;
+    private ICargar iCargar;
+    private IConsulta iConsulta;
+    private IPrincipalCom iPrincipalCom;
+    private IReportesCentro iReportesCentro;
+    private IReportesProf iReportesProf;
+    private IReportesTrabProf iReportesTrabProf;  
+    private Reporte reporte;
 }
